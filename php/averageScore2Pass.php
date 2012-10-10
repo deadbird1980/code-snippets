9a2451fb0c78ed6e4e796f47ca63018f4761a3e8
joey <joeyw@reallyenglish.com>
2012-10-11 00:13:19 +0100

    add different test cases

--- php/averageScore2Pass.php
+++ php/averageScore2Pass.php
@@ -4,28 +4,60 @@
  * pass the tests
  *
  */
-$score_to_pass = 60;
-$tests_to_pass = 5;
-$scores = array(100, 30, 10);
 
-rsort($scores);
-$i = 0;
-$sum = 0;
-foreach($scores as $score) {
-  if ($sum + $score < $score_to_pass * ($i+1)) {
-    break;
+function averageScore2Pass($config, $scores) {
+  $score_to_pass = $config['score_to_pass'];
+  $tests_to_pass = $config['tests_to_pass'];
+  $tests_no = $config['tests_no'];
+  print_r($config);
+
+  rsort($scores);
+  print_r($scores);
+  $i = 0;
+  $sum = 0;
+  foreach($scores as $score) {
+    if ($sum + $score < $score_to_pass * ($i+1)) {
+      break;
+    }
+    $sum += $score;
+    $i++;
   }
-  $sum += $score;
-  $i++;
-}
-$average_passed = $i;
-$tests_left = $tests_to_pass - count($scores);
-$average_score_to_pass = ($score_to_pass * $tests_to_pass - $sum)/($tests_left);
-$final_score = (array_sum($scores) + $tests_left * $average_score_to_pass);
-$sum_to_pass = $score_to_pass * $tests_to_pass;
-if ($final_score > $sum_to_pass) {
-  print "You need to pass $tests_left with average score of $average_score_to_pass\n";
-  print "final_score = $final_score, $sum_to_pass";
+  $average_passed = $i;
+  if ($average_passed < $tests_to_pass) {
+    $tests_left = $tests_no - count($scores);
+    $tests_required = $tests_to_pass - $average_passed;
+    if ($tests_left < $tests_required) {
+      for($j=0; $j<$tests_required - $tests_left; $j++) {
+        $sum += $scores[$i+$j];
+      }
+      $tests_required = $tests_left;
+    }
+    $sum_to_pass = $score_to_pass * $tests_to_pass;
+    $average_score_to_pass = ($sum_to_pass - $sum) / $tests_required;
+    print "You need to pass $tests_required with average score of $average_score_to_pass\n";
+    return $average_score_to_pass;
+  } else {
+    print "You passed by average score\n";
+  }
+  return 0;
 }
 
+$config = array('tests_no'=>4,
+                'score_to_pass'=>60,
+                'tests_to_pass'=>1
+            );
+$scores = array(60);
+averageScore2Pass($config, $scores);
+$config = array('tests_no'=>4,
+                'score_to_pass'=>60,
+                'tests_to_pass'=>3
+            );
+$scores = array(70, 60);
+averageScore2Pass($config, $scores);
+$scores = array(70, 30);
+averageScore2Pass($config, $scores);
+$scores = array(70, 30, 10);
+averageScore2Pass($config, $scores);
+$scores = array(40, 30, 10);
+averageScore2Pass($config, $scores);
 ?>
