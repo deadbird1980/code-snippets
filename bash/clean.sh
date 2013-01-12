8cfb8ea17c871efa56f599508c0ab5b2516dbc41
joey <joeyw@reallyenglish.com>
2013-01-12 22:11:38 +0000

    extention unknown added and template_111.swf covered as well

--- bash/clean.sh
+++ bash/clean.sh
@@ -13,17 +13,22 @@ then
     usage
     exit
 fi
+
 if [ ! -d "$1" ]
 then
     echo "Directory $1 does not exist!"
     exit
 fi
-SOURCE_DIR="$1"
 
+SOURCE_DIR="$1"
 TMP="/tmp"
-find $SOURCE_DIR/resource/file -type f \( -name "*.pdf" -or -name "*.jpg" -or -name "*.swf" -or -name "*.mp3" -or -name "*.png" \) | sed "s:$SOURCE_DIR/resource/::" > $TMP/all
-grep -o --no-filename 'file[^"]*\.\(pdf\|jpg\|swf\|mp3\)' $SOURCE_DIR/json/*.json| uniq | sed -e 's:\\/:/:g' >  $TMP/used
+
+# find out files under resource with extension as unknown, pdf, jpg, swf, mp3, png
+find $SOURCE_DIR/resource -type f \( -name "*.unknown" -or -name "*.pdf" -or -name "*.jpg" -or -name "*.swf" -or -name "*.mp3" -or -name "*.png" \) | sed "s:$SOURCE_DIR/resource/::" > $TMP/all
+grep -o --no-filename '"[^"]*\.\(pdf\|jpg\|swf\|mp3\|unknown\)"' $SOURCE_DIR/json/*.json| uniq |sed -e 's:"::g'| sed -e 's:\\/:/:g' >  $TMP/used
 fgrep -v $TMP/all -f $TMP/used > $TMP/unused
-rm $TMP/used
+
 rm $TMP/all
+rm $TMP/used
+rm $TMP/unused
 
