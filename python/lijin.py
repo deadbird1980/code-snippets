7b2427eb905dbb5d59d72519ca813613d6e3db22
joey <joeyw@reallyenglish.com>
2014-11-07 23:16:01 +0000

    download the file

--- python/lijin.py
+++ python/lijin.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python2.7
+# -*-coding:utf-8 -*
 import urllib
 import re
+import pycurl
 urls = ['http://www.ljgdw.com/tndb/class/']
 #urls = ['http://www.ljgdw.com/tndb/class/', 'http://www.ljgdw.com/wzxw/class/']
 for url in urls:
@@ -7,11 +10,22 @@ for url in urls:
   s = f.read()
   f.close()
   pages = re.findall('<a href="([^"]+)" class="newsquery" target="_self"[ ]+><li>([^<]+)<\/a>', s)
+  #pages = [['1.flv', u'http://shiping.ljgdw.com/flv/lj/11.03故事.flv']]
   for page in pages:
     f = urllib.urlopen(url + page[0])
     s = f.read()
     f.close()
     flv = re.search('<input type="text" name="filepath" value="([^"]+)"', s)
     if flv:
-      print page[1] + '   ' + flv.group(1)
-
+      url = flv.group(1)
+      #download the file
+      fp = open((url.rsplit('/',1))[1], "wb")
+      curl = pycurl.Curl()
+      url = urllib.quote(url).replace('%3A', ':')
+      print page[1] + '   ' + url
+      curl.setopt(pycurl.URL, url)
+      curl.setopt(pycurl.WRITEDATA, fp)
+      curl.perform()
+      curl.close()
+      fp.close()
+    break
