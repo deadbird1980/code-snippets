4099d3c9d6c8c9969e73c2d87dfee981cdb7fb67
joey <joeyw@reallyenglish.com>
2014-11-10 00:39:33 +0000

    pick up the date of today

--- python/dcxs.py
+++ python/dcxs.py
@@ -4,13 +4,15 @@ import urllib
 import re
 import pycurl
 import argparse
+from datetime import date
 parser = argparse.ArgumentParser(description='ADD YOUR DESCRIPTION HERE')
 parser.add_argument('-d','--date', help='Program date',required=False)
 parser.add_argument('-o','--destination', help='Download directory',required=False)
 parser.add_argument('-n','--dry-run', action="store_true", default=False, help='Dry run',required=False)
 args = parser.parse_args()
+d = date.yesterday().strftime('%Y%m%d')
 #url = 'http://res.audio.rbc.cn/mp3/xwgb/dcxs/dcxs2014110917205503.wma'
-url = 'http://audio.rbc.cn/play.form?programId=1413&start=20141109172000'
+url = 'http://audio.rbc.cn/play.form?programId=1413&start='+d+'172000'
 
 f = urllib.urlopen(url)
 s = f.read()
@@ -19,6 +21,7 @@ audio = re.search("var url = '(http://res.audio.rbc.cn/mp3/[^.]+.wma)';", s)
 if audio:
   url = audio.group(1)
   if url:
+    url = url.replace('wma', 'mp3')
     if args.dry_run:
       print 'Downloading ' + url
       exit(-1)
