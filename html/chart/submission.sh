c43258562eaa9bafea42d2c1c34bf54e91b4ff58
joey <joeyw@reallyenglish.com>
2016-05-06 18:22:50 +0100

    group by length of the course

--- html/chart/submission.sh
+++ html/chart/submission.sh
@@ -9,11 +9,14 @@ done
 
 #jq '[.[]|select(.duration==15724800)|.course_id as $course_id|.submissions[]|select(.point>0)|{"course_id":$course_id, "point":.point}]' -c submission.json > submission_flatten_6months.json
 #jq '[.[]|select(.duration==28425600)|.course_id as $course_id|.submissions[]|select(.point>0)|{"course_id":$course_id, "point":.point}]' -c submission.json > submission_flatten_11months.json
-28425600
 
 for i in "${units[@]}"
 do
   echo $i
   bash -c "jq '${i} as \$unit|group_by(.point/\$unit|floor)|[.[]|{key:(.[0].point/\$unit|floor*\$unit|tostring),value:length}]|from_entries' submission_flatten_11months.json" > "submission_result_11months_$i".json
 done
+
+jq '1 as $group|1 as $unit|[.[]|select(.duration!=null)|{group:(.duration/(3600*24*$group)|floor+1),submissions}]|group_by(.group)|[.[]|{key:.[0].group|tostring,value:[.[]|.submissions[]|select(.point!=null)]|group_by(.point/$unit|floor)|[.[]|{key:(.[0].point/$unit|floor*$unit|tostring),value:length}]|from_entries}]|from_entries' submission.json > day_submission.json
+jq '7 as $group|1 as $unit|[.[]|select(.duration!=null)|{group:(.duration/(3600*24*$group)|floor+1),submissions}]|group_by(.group)|[.[]|{key:.[0].group|tostring,value:[.[]|.submissions[]|select(.point!=null)]|group_by(.point/$unit|floor)|[.[]|{key:(.[0].point/$unit|floor*$unit|tostring),value:length}]|from_entries}]|from_entries' submission.json > week_submission.json
+jq '30 as $group|1 as $unit|[.[]|select(.duration!=null)|{group:(.duration/(3600*24*$group)|floor+1),submissions}]|group_by(.group)|[.[]|{key:.[0].group|tostring,value:[.[]|.submissions[]|select(.point!=null)]|group_by(.point/$unit|floor)|[.[]|{key:(.[0].point/$unit|floor*$unit|tostring),value:length}]|from_entries}]|from_entries' submission.json > month_submission.json
 
