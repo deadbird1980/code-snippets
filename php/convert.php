0d391c40620261f1d1de6ab3724f5a41ab6a7373
joey <joeyw@reallyenglish.com>
2020-03-20 14:51:19 +0000

    section section completed

--- php/convert.php
+++ php/convert.php
@@ -100,7 +100,6 @@ function convert() {
               }
               $choices[] = $choice;
             }
-            $question['question'] = array("text" => "Question $q_index");
             $question['choices'] = $choices;
             $questions[] = $question;
           }
@@ -109,21 +108,28 @@ function convert() {
           $activity['prompt'] = $prompt;
           $activities[] = $activity;
         }
-      } else if (strpos($content->type, 'DTOQuestionUnitErrorRecognitionITP') != false || strpos($content->type, 'DTOQuestionUnitErrorRecognitionITP') != false) {
+      } else if (strpos($content->type, 'DTOSectionPartitionErrorRecognitionITP') != false || strpos($content->type, 'DTOSectionPartitionSentenceCompletionITP') != false) {
         $q_index = 0;
         foreach($content->data->contents as $c) {
           if (strpos($c->type, 'DTOQuestionUnitErrorRecognitionITP') == false && strpos($c->type, 'DTOQuestionUnitSentenceCompletionITP') == false) {
             continue;
+          }
+          $is_sentence_completion = true;
+          if (strpos($c->type, 'DTOQuestionUnitSentenceCompletionITP') == false) {
+            $is_sentence_completion = false;
           }
           $activity = array();
           $activity['activity_name'] = 'text_with_gap_one_of_many';
           $prompt = array();
           $questions = array();
           $prompt['shuffle'] = false;
+          if ($is_sentence_completion) {
+            $prompt['instruction'] = 'Complete the sentence.';
+          } else {
+            $prompt['instruction'] = 'Identify the word or phrase that must be changed in order for the sentence to be correct.';
+          }
 
           // add master data into stimulus.instruction
-          $stimulus['instruction'] = 'Listen to the conversation and answer the questions.';
-          $stimulus['audio'] = array("transcript"=>"*", "source"=>$c->audio->data->resource->data->resourceID);
           foreach($c->data->questions as $q) {
             $q = $q->data;
             $question = array();
@@ -131,10 +137,15 @@ function convert() {
             $question['marker'] = 1;
             $choices = array();
             $question['explanation'] = $q->explanation->data->text->data->studyLanguage;
+            $question['question'] = array("text" => $q->sentence);
+            if (!$is_sentence_completion) {
+              preg_match_all('/\[([^\]]+)\]/', $q->sentence, $matches);
+              $question['text'] = preg_replace('/\[([^\]]+)\]/', '[*\1]', $q->sentence);
+            }
             $correct = $q->correctAnswer->data->optionIdentifier;
-            foreach($q->options as $option) {
+            foreach($q->options as $inx => $option) {
               $choice = array();
-              $choice['text'] = $option->data->text;
+              $choice['text'] = $option->data->text? $option->data->text : $matches[1][$inx];
               if ($correct == $option->data->identifier) {
                 $choice['correct'] = true;
               } else {
@@ -142,7 +153,6 @@ function convert() {
               }
               $choices[] = $choice;
             }
-            $question['question'] = array("text" => "Question $q_index");
             $question['choices'] = $choices;
             $questions[] = $question;
           }
