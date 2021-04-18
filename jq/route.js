699886c26374ec38fcf0bb54d364845c678eafe2
Joey Wang <joeyw@Joeys-M1-Pro.local>
2021-04-18 18:48:07 +0100

    use recusive to find the route

--- jq/route.js
+++ jq/route.js
@@ -1,35 +1,31 @@
 var tab = [{"p":"1","c":"2"},{"p":"1","c":"3"},{"p":"2","c":"4"},{"p":"3","c":"5"},{"p":"5","c":"6"},{"p":"5","c":"7"},{"p":"5","c":"8"},{"p":"8","c":"9"}];
-function route(arr) {
-  var rtn=[], r = [], fnd = false;
+function group(arr) {
+  var tree = {};
   for(var i in arr) {
-  	console.log('i=', i);
-  	for(var j in rtn) {
-  		var indx = rtn[j].indexOf(arr[i].p);
-  		console.log('index=', indx);
-  		if (indx >= 0) {
-  			if (indx + 1 == rtn[j].length) {
-  			  rtn[j].push(arr[i].c);
-  			} else {
-  			  r = rtn[j].slice(0, indx-2);
-  			}
-  			fnd = true;
-  			
-  		} else {
-  			fnd = false;
-  			r = [arr[i].p, arr[i].c];
-  		}
-  	}
-  	if (fnd) {
-        r.push(arr[i].p);
-        r.push(arr[i].c);
+  	if (!tree[arr[i].p]) {
+  		tree[arr[i].p] = [arr[i].c];
+
   	} else {
-  		r.push(arr[i].p);
-        r.push(arr[i].c);
+  		tree[arr[i].p].push(arr[i].c);
   	}
-  	
-  	rtn.push(r);
   }
-  console.log('rtn=', rtn);
+  return tree;
+}
+var routes = [];
+function route_from(arr, key, route=[]) {
+	route.push(key);
+	if (!arr[key]) {
+		routes.push(route);
+		return [];
+	}
+
+	var val = arr[key];
+	for(var i in val) {
+		var nroute = route.filter(function(){return true});
+		route_from(arr, val[i], nroute);
+	}
 }
 
-route(tab);
+var group = group(tab);
+route_from(group, '1');
+console.log(routes);
